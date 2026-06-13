05d29ee763c457863b072c16e0e6b8b149a62cf5
Moubarak Congacou <cmouba1999@gmail.com>
2026-06-13 11:51:23 -0400

    Initial Spring Boot Files

---
+++ src/main/java/com/pluralsight/northwind_traders_springboot/NorthwindTradersSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind_traders_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTradersSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTradersSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/northwind_traders_springboot/NorthwindTradersSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.northwind_traders_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
